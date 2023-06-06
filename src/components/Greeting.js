4aca7a4d64732e1d49e3729345048bb804ee247a
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-06 12:15:45 +0700

    demo1

---
+++ src/components/Greeting.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>Welcome!</h3>
+      <p>Men</p>
+    </div>
+  )
+}
+
+export default Greeting
