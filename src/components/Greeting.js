71c781c45492fa143d5a6a38d3e62bbe1b43d701
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-06 18:44:03 +0700

    conditional rendering and event handler

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,11 +4,12 @@ import classes from './Greeting.module.css'
 const Greeting = (props) => {
   // const name = 'John'
   const { name } = props
+  const isLogin = true
 
   return (
     <div className={classes.card}>
       <h3>Welcome!</h3>
-      <p>{name}</p>
+      <p>{isLogin ? name : 'Unknown'}</p>
     </div>
   )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,8 +4,12 @@ import classes from './Post.module.css'
 const Post = (props) => {
   const { post } = props
 
+  const handleClick = (id) => {
+    alert(`Post id: ${id} clicked`)
+  }
+
   return (
-    <div className={classes.post}>
+    <div onClick={() => handleClick(post.id)} className={classes.post}>
       <p>id: {post.id}</p>
       <p>title: {post.title}</p>
     </div>
