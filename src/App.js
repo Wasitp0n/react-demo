28e616b958281f05a1bb2b55c78bffb6b7a6d233
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-07 17:06:24 +0700

    form input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 import Post from './components/Post'
 
 // * Data from database
-const posts = [
+const initPosts = [
   {
     id: 1,
     title: "Let's learn React!",
@@ -21,10 +21,33 @@ const posts = [
 ]
 
 function App() {
+  const [newPostTitle, setNewPostTitle] = useState('')
+  const [posts, setNewPosts] = useState(initPosts)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setNewPosts([
+      ...posts,
+      {
+        id: Math.floor(Math.random() * 1000), // * database should auto generate id
+        title: newPostTitle,
+      },
+    ])
+
+    setNewPostTitle('')
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Greeting />
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input type="text" value={newPostTitle} onChange={(e) => setNewPostTitle(e.target.value)} required />
+
+        <input type="submit" value="Post" />
+      </form>
       <div className="feed-container">
         {posts.map((post) => {
           return <Post key={post.id} post={post} />
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Post.module.css'
 
 const Post = (props) => {
   const { post } = props
+  const [isShow, setIsShow] = useState(false)
+
+  const toggleShow = () => {
+    setIsShow(!isShow)
+  }
 
   return (
     <div className={classes.post}>
       <p>id: {post.id}</p>
       <p>title: {post.title}</p>
+      {isShow && <p>more post information...</p>}
+      <button onClick={toggleShow}>{isShow ? 'Show Less' : 'Show More'}</button>
     </div>
   )
 }
