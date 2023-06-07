babb677b9ab4cb14cb406ccd6a53b3cbc44d2863
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-07 15:02:17 +0700

    remove unused func

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,12 +4,8 @@ import classes from './Post.module.css'
 const Post = (props) => {
   const { post } = props
 
-  const handleClick = (id) => {
-    alert(`Post id: ${id} clicked`)
-  }
-
   return (
-    <div onClick={() => handleClick(post.id)} className={classes.post}>
+    <div className={classes.post}>
       <p>id: {post.id}</p>
       <p>title: {post.title}</p>
     </div>
